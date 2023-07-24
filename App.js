1630311d5ac383a78e6f3c3a38ab6237422f887a
Lakshit sharma <lakshlakshit18@gmail.com>
2023-07-24 12:51:34 +0530

    init:commit

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet, Text, View, NativeEventEmitter,
   PermissionsAndroid, Platform, NativeModules,
 } from 'react-native';
 import { Landing } from './Pages/Landing';
 import BleManager from 'react-native-ble-manager';
+import { NavigationContainer } from '@react-navigation/native';
 
 
-export default function App() {
+
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+const App = () => {
+  const peripherals = new Map();
+
+  const [connected, setConnected] = useState(false);
+  const [isScanning, setIsScanning] = useState(false);
+
+
+
+
   useEffect(() => {
     // turn on bluetooth if it is not on
     BleManager.enableBluetooth().then(() => {
@@ -26,6 +38,12 @@ export default function App() {
         handleGetConnectedDevices();
       },
     );
+    let getavailableDevices = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      (abc) => {
+        console.log(abc)
+      },
+    )
     if (Platform.OS === 'android' && Platform.Version >= 23) {
       PermissionsAndroid.check(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -45,82 +63,47 @@ export default function App() {
         }
       });
     }
-    const startScan = () => {
-      if (!isScanning) {
-        BleManager.scan([], 5, true)
-          .then(() => {
-            setIsScanning(true);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    };
-    const handleGetConnectedDevices = () => {
-      BleManager.getConnectedPeripherals([]).then(results => {
-        if (results.length == 0) {
-          console.log('No connected bluetooth devices');
-        } else {
-          for (let i = 0; i < results.length; i++) {
-            let peripheral = results[i];
-            peripheral.connected = true;
-            peripherals.set(peripheral.id, peripheral);
-            setConnected(true);
-            setBluetoothDevices(Array.from(peripherals.values()));
-          }
-        }
-      });
-    };
-    const connectToPeripheral = peripheral => {
-      if (peripheral.connected) {
-        BleManager.disconnect(peripheral.id).then(() => {
-          peripheral.connected = false;
-          setConnected(false);
-          alert(`Disconnected from ${peripheral.name}`);
-        });
-      } else {
-        BleManager.connect(peripheral.id)
-          .then(() => {
-            let peripheralResponse = peripherals.get(peripheral.id);
-            if (peripheralResponse) {
-              peripheralResponse.connected = true;
-              peripherals.set(peripheral.id, peripheralResponse);
-              setConnected(true);
-              setBluetoothDevices(Array.from(peripherals.values()));
-            }
-            alert('Connected to ' + peripheral.name);
-          })
-          .catch(error => console.log(error));
-        /* Read current RSSI value */
-        setTimeout(() => {
-          BleManager.retrieveServices(peripheral.id).then(peripheralData => {
-            console.log('Peripheral services:', peripheralData);
-          });
-        }, 900);
-      }
-    };
-  
     return () => {
+      getavailableDevices,
       stopListener.remove();
     };
   }, []);
-  return (
-    <Landing />
+  
 
+
+  const handleGetConnectedDevices = () => {
+    BleManager.getConnectedPeripherals([]).then(results => {
+      console.log(results)
+      if (results.length == 0) {
+        console.log('No connected bluetooth devices');
+      } else {
+        for (let i = 0; i < results.length; i++) {
+          let peripheral = results[i];
+          peripheral.connected = true;
+          peripherals.set(peripheral.id, peripheral);
+          setConnected(true);
+          setBluetoothDevices(Array.from(peripherals.values()));
+        }
+      }
+    });
+  };
+
+  return (
+    <NavigationContainer>
+      <Landing />
+    </NavigationContainer>
   );
 
-
-
-  
- const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  const BleManagerModule = NativeModules.BleManager;
-  const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 }
-  export default function App()
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default App;
